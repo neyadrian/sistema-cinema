faf8159bdab10e7a4ec9dce7beee488c9cff4a0f
Ney Adrian <neyadrian2018@gmail.com>
2026-02-11 10:51:28 -0300

    sistema de cinema - finalizado

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,14 +1,5 @@
 import java.util.Scanner;
 
-/*Crie um sistema para gerenciar assentos de uma sala de cinema.
-Requisitos:
-A sala deve ser representada por uma matriz de String ou char (ex: 10x10).
-Inicialmente, todos os assentos estão "livres" (-).
-O usuário deve poder escolher uma fileira e uma poltrona via Scanner.
-Regra de Negócio: Se o assento estiver ocupado, o sistema deve avisar e pedir nova entrada. Se estiver livre, marque com X.
-Evolução OO: Não faça tudo na main. Crie uma classe Cinema que possui a matriz como atributo privado (private) e métodos reservar(int fileira, int cadeira) e mostrarMapa().
-*/
-
 public class Main {
     public static void main(String[] args) {
 
@@ -41,7 +32,6 @@ public class Main {
                 }
             }
         } while (op != 0);
-
 
         sc.close();
     }
